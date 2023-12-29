7a6dc11a68857d864f020f1efa05e77ddc2dc6cc
maryemjoudir <maryemjoudir02@gmail.com>
2023-12-29 21:59:33 +0100

    assets files

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -6,10 +6,10 @@ export default function Login() {
       <div class="container-fluid page-body-wrapper full-page-wrapper">
         <div class="content-wrapper d-flex align-items-center auth px-0">
           <div class="row w-100 mx-0">
-            <div class="col-lg-4 mx-auto">
+            <div class="col-lg-12 mx-auto">
               <div class="auth-form-light text-left py-5 px-4 px-sm-5">
                 <div class="brand-logo">
-                  <img src="../../assets/images/logo.svg" alt="logo" />
+                <img src=".././src/assets/images/logo.svg" alt="logo" />
                 </div>
                 <h4>Hello! let's get started</h4>
                 <h6 class="fw-light">Sign in to continue.</h6>
